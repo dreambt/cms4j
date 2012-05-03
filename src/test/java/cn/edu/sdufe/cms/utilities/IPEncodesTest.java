cd9d8fdfc5cd0c77bc1f0a17a0e2991b1759fec1
baitao.jibt <dreambt@gmail.com>
2012-05-03 11:04:10 +0000

    Test
    git-svn-id: http://192.168.1.227/svn/CMS4j/trunk@391 d1d3f527-36cb-4749-94ba-585de1865e2b

--- src/test/java/cn/edu/sdufe/cms/utilities/IPEncodesTest.java
+++ src/test/java/cn/edu/sdufe/cms/utilities/IPEncodesTest.java
@@ -23,12 +23,12 @@ public class IPEncodesTest {
         ipEncodes = new IPEncodes();
     }
 
-    @Test(timeout = 5)
+    @Test(timeout = 50)
     public void testIpToLong() throws Exception {
         assertEquals(address, ipEncodes.ipToLong(HOSTNAME));
     }
 
-    @Test(timeout = 5)
+    @Test(timeout = 50)
     public void testLongToIp() throws Exception {
         assertEquals(HOSTNAME, ipEncodes.longToIp(address));
     }
